24347d6ff7d5a0ee9452fa6e3977cb655e0ff574
Connor Slade <connor@connorcode.com>
2022-08-27 19:40:36 -0400

    Process info on api

--- src/api/routes/app.rs
+++ src/api/routes/app.rs
@@ -15,16 +15,23 @@ pub fn attach(server: &mut Server<Arc<App>>) {
             None => return misc::error_res("Invalid App"),
         };
 
+        // Get std(out|err)
         let stdout = app.process.stdout.read();
         let stdout = String::from_utf8_lossy(&stdout);
-
         let stderr = app.process.stderr.read();
         let stderr = String::from_utf8_lossy(&stderr);
+
+        // Get info on the process
+        let pid = app.process.process.lock().as_ref().unwrap().id() as i32;
+        let mem_info = procinfo::pid::statm(pid).unwrap();
+        let stats = procinfo::pid::stat(pid).unwrap();
 
         Response::new()
             .text(json!({
                 "name": app.name,
                 "status": app.status.read().json(),
+                "memory": mem_info.size,
+                "threads": stats.num_threads,
                 "output": {
                     "stdout": stdout,
                     "stderr": stderr,
